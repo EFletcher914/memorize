ce6316b0f9ea85579336cae46bc9ac8dc0da1b1d
Eric Fletcher <insitefule@gmail.com>
2024-01-18 10:27:34 -0500

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Eric Fletcher on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
